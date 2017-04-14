413cfbf2da941c4dd01a03f72de2add22db47c36
SichYuriy <sichyuriyy@gmail.com>
2017-04-14 15:50:26 +0300

    added some helper methods to facilitate security building

--- src/main/java/com/gmail/at/sichyuriyy/onlinestore/dispatcher/HttpMethod.java
+++ src/main/java/com/gmail/at/sichyuriyy/onlinestore/dispatcher/HttpMethod.java
@@ -1,8 +1,15 @@
 package com.gmail.at.sichyuriyy.onlinestore.dispatcher;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by Yuriy on 4/4/2017.
  */
 public enum HttpMethod {
-    GET, PUT, POST, DELETE
+    GET, PUT, POST, DELETE;
+
+    public static List<HttpMethod> all() {
+        return Arrays.asList(HttpMethod.values());
+    }
 }
--- src/main/java/com/gmail/at/sichyuriyy/onlinestore/entity/Role.java
+++ src/main/java/com/gmail/at/sichyuriyy/onlinestore/entity/Role.java
@@ -1,8 +1,15 @@
 package com.gmail.at.sichyuriyy.onlinestore.entity;
 
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Yuriy on 3/27/2017.
  */
 public enum Role {
-    CUSTOMER, ADMINISTRATOR
+    CUSTOMER, ADMINISTRATOR;
+
+    public static List<Role> adminRoles() {
+        return Collections.singletonList(ADMINISTRATOR);
+    }
 }
